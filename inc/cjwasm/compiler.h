ec5f76ea7ee941286ae8d686770305eae258d714
James Anhalt <jeaiii@anhalt.org>
2022-11-12 15:03:12 -0800

    fixes op_return stack offsets fixes byte code for src2 removes wasm enum scope

--- inc/cjwasm/compiler.h
+++ inc/cjwasm/compiler.h
@@ -9,39 +9,36 @@ using int64_t = long long;
 
 namespace cjwasm
 {
-    struct wasm
+    enum : uint8_t
     {
-        enum : uint8_t
-        {
-            op_unreachable, op_nop, op_block, op_loop, op_if, op_else,
-            op_end = 0x0b, op_br, op_br_if, op_return = 0x0f,
-            op_call = 0x10,
+        op_unreachable, op_nop, op_block, op_loop, op_if, op_else,
+        op_end = 0x0b, op_br, op_br_if, op_return = 0x0f,
+        op_call = 0x10,
 
-            op_drop = 0x1a, op_select,
+        op_drop = 0x1a, op_select,
 
-            op_local_get = 0x20, op_local_set, op_local_tee,
+        op_local_get = 0x20, op_local_set, op_local_tee,
 
-            op_const_i32 = 0x41,
-            op_const_i64,
+        op_const_i32 = 0x41,
+        op_const_i64,
 
-            op_eqz_i32 = 0x45,
-            op_eq_i32,
-            op_ne_i32,
-            op_lt_s_i32,
-            op_lt_u_i32,
-            op_gt_s_i32,
-            op_gt_u_i32,
-            op_le_s_i32,
-            op_le_u_i32,
-            op_ge_s_i32,
-            op_ge_u_i32,
+        op_eqz_i32 = 0x45,
+        op_eq_i32,
+        op_ne_i32,
+        op_lt_s_i32,
+        op_lt_u_i32,
+        op_gt_s_i32,
+        op_gt_u_i32,
+        op_le_s_i32,
+        op_le_u_i32,
+        op_ge_s_i32,
+        op_ge_u_i32,
 
-            op_add_i32 = 0x6a, op_sub_i32, op_mul_i32, op_div_s_i32, op_div_u_i32, op_rem_s_i32, op_rem_u_i32,
-            op_and_i32, op_or_i32, op_xor_i32,
+        op_add_i32 = 0x6a, op_sub_i32, op_mul_i32, op_div_s_i32, op_div_u_i32, op_rem_s_i32, op_rem_u_i32,
+        op_and_i32, op_or_i32, op_xor_i32,
 
-            bt_void = 0x40,
-            bt_i32 = 0x7f, bt_i64 = 0x7e, bt_f32 = 0x7d, bt_f64 = 0x7c,
-        };
+        bt_void = 0x40,
+        bt_i32 = 0x7f, bt_i64 = 0x7e, bt_f32 = 0x7d, bt_f64 = 0x7c,
     };
 
     union value_t
@@ -145,7 +142,7 @@ namespace cjwasm
         // unconditional branches don't depend on stack depth so handle them outside compile so there aren't unique ones per stack depth
         void do_branch(block& scope)
         {
-            if (scope.op == wasm::op_loop)
+            if (scope.op == op_loop)
             {
                 // branch back
                 emit([](ip_t ip, sp_t sp) { auto offset = operand_u32(ip); (ip - offset + 1)->code(ip - offset + 2, sp); }, dst - scope.enter + 2);
@@ -166,11 +163,11 @@ namespace cjwasm
             {
                 emit([](ip_t ip, sp_t sp)
                     {
-                        auto return_ip = sp[N - 2].ip;
-                        auto return_sp = sp[N - 1].sp;
                         auto n = ip[0].u32;
+                        auto return_ip = sp[N + 1 - 2 - n].ip;
+                        auto return_sp = sp[N + 1 - 1 - n].sp;
                         for (uint32_t i = 0; i < n; ++i)
-                            sp[i] = sp[N - n + i + 1];
+                            sp[i] = sp[N + 1 - n + i];
 
                         return_ip->code(return_ip + 1, return_sp);
                     }, uint32_t(self->return_count));
@@ -178,28 +175,31 @@ namespace cjwasm
 
             while (n == N) switch (get_code())
             {
-            case wasm::op_nop:
+            case op_unreachable:
+                    continue;
+
+            case op_nop:
                 continue;
 
-            case wasm::op_block:
-                *--bp = { wasm::op_block, get_u8(), dst, nullptr };
+            case op_block:
+                *--bp = { op_block, get_u8(), dst, nullptr };
                 continue;
-            case wasm::op_loop:
-                *--bp = { wasm::op_loop, get_u8(), dst, nullptr };
+            case op_loop:
+                *--bp = { op_loop, get_u8(), dst, nullptr };
                 continue;
-            case wasm::op_if:
-                *--bp = { wasm::op_if, get_u8(), dst, nullptr };
+            case op_if:
+                *--bp = { op_if, get_u8(), dst, nullptr };
                 emit(do_forward_if, dst - dst);
                 return N - 1;
 
-            case wasm::op_else:
+            case op_else:
                 // fix if branch
                 (uint32_t&)bp[0].enter[1] = uint32_t(dst - bp[0].enter);
                 // branch to leave
                 do_branch(bp[0]);
                 continue;
 
-            case wasm::op_end:
+            case op_end:
                 // fix up forward branches
                 for (ip_t np = bp[0].leave; np != 0;)
                 {
@@ -217,14 +217,14 @@ namespace cjwasm
                 ++bp;
                 continue;
 
-            case wasm::op_br:
+            case op_br:
                 do_branch(bp[get_leb128_u32()]);
                 continue;
 
-            case wasm::op_br_if:
+            case op_br_if:
             {
                 auto& scope = bp[get_leb128_u32()];
-                if (scope.op == wasm::op_loop)
+                if (scope.op == op_loop)
                 {
                     // branch back
                     emit([](ip_t ip, sp_t sp) { auto offset = sp[N].i32 == 0 ? 0 : operand_u32(ip); (ip - offset + 1)->code(ip - offset + 2, sp); }, dst - scope.enter + 2);
@@ -237,10 +237,10 @@ namespace cjwasm
                 return N - 1;
             }
 
-            case wasm::op_return:
+            case op_return:
                 emit_return();
                 continue;
-            case wasm::op_call:
+            case op_call:
             {
                 auto const& f = functions[get_leb128_u32()];
                 emit([](ip_t ip, sp_t sp)
@@ -258,52 +258,52 @@ namespace cjwasm
                 n += f.return_count - f.argument_count;
                 continue;
             }
-            case wasm::op_drop: return N - 1;
-            case wasm::op_select:
+            case op_drop: return N - 1;
+            case op_select:
                 emit([](ip_t ip, sp_t sp) { if (sp[N].i32 != 0) sp[N - 2] = sp[N - 1]; ip->code(ip + 1, sp); });
                 return N - 2;
-            case wasm::op_local_get:
+            case op_local_get:
                 emit([](ip_t ip, sp_t sp) { sp[N + 1] = sp[operand_u32(ip)]; (ip + 1)->code(ip + 2, sp); }, get_leb128_u32());
                 return compile<N + 1>(N + 1);
-            case wasm::op_local_set:
+            case op_local_set:
                 emit([](ip_t ip, sp_t sp) { sp[operand_u32(ip)] = sp[N]; (ip + 1)->code(ip + 2, sp); }, get_leb128_u32());
                 return N - 1;
-            case wasm::op_local_tee:
+            case op_local_tee:
                 emit([](ip_t ip, sp_t sp) { sp[operand_u32(ip)] = sp[N]; (ip + 1)->code(ip + 2, sp); }, get_leb128_u32());
                 continue;
 
-            case wasm::op_const_i32:
+            case op_const_i32:
                 emit([](ip_t ip, sp_t sp) { sp[N + 1].i32 = ip->i32; (ip + 1)->code(ip + 2, sp); }, get_leb128_i32());
                 return compile<N + 1>(N + 1);
-            case wasm::op_const_i64:
+            case op_const_i64:
                 emit([](ip_t ip, sp_t sp) { sp[N + 1].i64 = ip->i64; (ip + 1)->code(ip + 2, sp); }, get_leb128_u64());
                 return compile<N + 1>(N + 1);
 
-            case wasm::op_eqz_i32: emit([](ip_t ip, sp_t sp) { sp[N].u32 = sp[N].u32 == 0 ? 1 : 0; ip->code(ip + 1, sp); }); continue;
+            case op_eqz_i32: emit([](ip_t ip, sp_t sp) { sp[N].u32 = sp[N].u32 == 0 ? 1 : 0; ip->code(ip + 1, sp); }); continue;
 
-            case wasm::op_eq_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 == sp[N].u32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
-            case wasm::op_ne_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 != sp[N].u32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
+            case op_eq_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 == sp[N].u32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
+            case op_ne_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 != sp[N].u32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
 
-            case wasm::op_lt_s_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].i32 = sp[N - 1].i32 < sp[N].i32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
-            case wasm::op_lt_u_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 < sp[N].u32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
-            case wasm::op_gt_s_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].i32 = sp[N - 1].i32 > sp[N].i32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
-            case wasm::op_gt_u_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 > sp[N].u32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
-            case wasm::op_le_s_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].i32 = sp[N - 1].i32 <= sp[N].i32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
-            case wasm::op_le_u_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 <= sp[N].u32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
-            case wasm::op_ge_s_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].i32 = sp[N - 1].i32 >= sp[N].i32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
-            case wasm::op_ge_u_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 >= sp[N].u32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
+            case op_lt_s_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].i32 = sp[N - 1].i32 < sp[N].i32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
+            case op_lt_u_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 < sp[N].u32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
+            case op_gt_s_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].i32 = sp[N - 1].i32 > sp[N].i32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
+            case op_gt_u_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 > sp[N].u32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
+            case op_le_s_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].i32 = sp[N - 1].i32 <= sp[N].i32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
+            case op_le_u_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 <= sp[N].u32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
+            case op_ge_s_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].i32 = sp[N - 1].i32 >= sp[N].i32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
+            case op_ge_u_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 >= sp[N].u32 ? 1 : 0; ip->code(ip + 1, sp); }), N - 1;
 
-            case wasm::op_add_i32:   return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 + sp[N].u32; ip->code(ip + 1, sp); }), N - 1;
-            case wasm::op_sub_i32:   return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 - sp[N].u32; ip->code(ip + 1, sp); }), N - 1;
-            case wasm::op_mul_i32:   return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 * sp[N].u32; ip->code(ip + 1, sp); }), N - 1;           
-            case wasm::op_div_s_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].i32 = sp[N - 1].i32 / sp[N].i32; ip->code(ip + 1, sp); }), N - 1;
-            case wasm::op_div_u_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 / sp[N].u32; ip->code(ip + 1, sp); }), N - 1;
-            case wasm::op_rem_s_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].i32 = sp[N - 1].i32 % sp[N].i32; ip->code(ip + 1, sp); }), N - 1;
-            case wasm::op_rem_u_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 % sp[N].u32; ip->code(ip + 1, sp); }), N - 1;
+            case op_add_i32:   return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 + sp[N].u32; ip->code(ip + 1, sp); }), N - 1;
+            case op_sub_i32:   return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 - sp[N].u32; ip->code(ip + 1, sp); }), N - 1;
+            case op_mul_i32:   return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 * sp[N].u32; ip->code(ip + 1, sp); }), N - 1;           
+            case op_div_s_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].i32 = sp[N - 1].i32 / sp[N].i32; ip->code(ip + 1, sp); }), N - 1;
+            case op_div_u_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 / sp[N].u32; ip->code(ip + 1, sp); }), N - 1;
+            case op_rem_s_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].i32 = sp[N - 1].i32 % sp[N].i32; ip->code(ip + 1, sp); }), N - 1;
+            case op_rem_u_i32: return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 % sp[N].u32; ip->code(ip + 1, sp); }), N - 1;
 
-            case wasm::op_and_i32:   return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 & sp[N].u32; ip->code(ip + 1, sp); }), N - 1;
-            case wasm::op_or_i32:    return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 | sp[N].u32; ip->code(ip + 1, sp); }), N - 1;
-            case wasm::op_xor_i32:   return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 ^ sp[N].u32; ip->code(ip + 1, sp); }), N - 1;
+            case op_and_i32:   return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 & sp[N].u32; ip->code(ip + 1, sp); }), N - 1;
+            case op_or_i32:    return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 | sp[N].u32; ip->code(ip + 1, sp); }), N - 1;
+            case op_xor_i32:   return emit([](ip_t ip, sp_t sp) { sp[N - 1].u32 = sp[N - 1].u32 ^ sp[N].u32; ip->code(ip + 1, sp); }), N - 1;
             }
             while (n > N)
                 n = compile<N + 1>(n);
@@ -324,7 +324,7 @@ namespace cjwasm
             dst_end = dst + dst_size;
 
             bp = &blocks[15];
-            *bp = { wasm::op_block, wasm::bt_i32, dst, nullptr };
+            *bp = { op_block, bt_i32, dst, nullptr };
 
             function fs[8];
             functions = fs;
@@ -332,7 +332,7 @@ namespace cjwasm
             functions[0] = { dst, n, 1 };
             self = &functions[0];
 
-            compile<2>(1 + n);
+            compile<2>(n + 1);
             return dst - dst_begin;
         }
     };
--- test/main.cpp
+++ test/main.cpp
@@ -2,12 +2,12 @@
 
 uint8_t src1[]
 { 
-    cjwasm::wasm::op_local_get, 0, 
-    cjwasm::wasm::op_local_get, 1, 
-    cjwasm::wasm::op_mul_i32, 
-    cjwasm::wasm::op_local_get, 0, 
-    cjwasm::wasm::op_add_i32,
-    cjwasm::wasm::op_end
+    cjwasm::op_local_get, 0, 
+    cjwasm::op_local_get, 1, 
+    cjwasm::op_mul_i32, 
+    cjwasm::op_local_get, 0, 
+    cjwasm::op_add_i32,
+    cjwasm::op_end
 };
 
 /*
@@ -42,28 +42,28 @@ get_local $p1)
 
 uint8_t src2[]
 {
-    cjwasm::wasm::op_block, cjwasm::wasm::bt_void,
-    cjwasm::wasm::op_local_get, 1,
-    cjwasm::wasm::op_const_i32, 2,
-    cjwasm::wasm::op_lt_s_i32,
-    cjwasm::wasm::op_br_if, 0,
+    cjwasm::op_block, cjwasm::bt_void,
+    cjwasm::op_local_get, 0,
+    cjwasm::op_const_i32, 2,
+    cjwasm::op_lt_s_i32,
+    cjwasm::op_br_if, 0,
 
-    cjwasm::wasm::op_loop, cjwasm::wasm::bt_void,
-    cjwasm::wasm::op_local_get, 1,
-    cjwasm::wasm::op_local_get, 0,
-    cjwasm::wasm::op_mul_i32,
-    cjwasm::wasm::op_local_set, 0,
-    cjwasm::wasm::op_local_get, 1,
-    cjwasm::wasm::op_const_i32, uint8_t(-1),
-    cjwasm::wasm::op_add_i32,
-    cjwasm::wasm::op_local_tee, 1,
-    cjwasm::wasm::op_const_i32, 1,
-    cjwasm::wasm::op_gt_s_i32, 0,
-    cjwasm::wasm::op_br_if, 0,
-    cjwasm::wasm::op_end,
-    cjwasm::wasm::op_end,
-    cjwasm::wasm::op_local_get, 0,
-    cjwasm::wasm::op_end,
+    cjwasm::op_loop, cjwasm::bt_void,
+    cjwasm::op_local_get, 1,
+    cjwasm::op_local_get, 0,
+    cjwasm::op_mul_i32,
+    cjwasm::op_local_set, 1,
+    cjwasm::op_local_get, 0,
+    cjwasm::op_const_i32, uint8_t(-1),
+    cjwasm::op_add_i32,
+    cjwasm::op_local_tee, 0,
+    cjwasm::op_const_i32, 1,
+    cjwasm::op_gt_s_i32,
+    cjwasm::op_br_if, 0,
+    cjwasm::op_end,
+    cjwasm::op_end,
+    cjwasm::op_local_get, 1,
+    cjwasm::op_end,
 };
 
 cjwasm::code_t g_dst[1024];
@@ -83,5 +83,5 @@ int main()
         return cjwasm::call<int>(g_dst, x, y);
     };
 
-    return mul_add(10, 5);
+    return mul_add(5, 5);
 }
